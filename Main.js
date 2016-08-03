8df76377acde83d970db4e0755fd77d85ea1824c
Joshua Weinstein <jweinstein@splunk.com>
2016-08-03 11:16:51 -0700

    variable system

--- Main.js
+++ Main.js
@@ -1,3 +1,10 @@
 /**
  * Created by Josh on 8/2/16.
  */
+var names = require("./src/variables/variableset.js");
+
+var test = new names.VariableSet();
+
+console.log(test.newVar());
+console.log(test.newVar());
+console.log(test.pickVar());
--- src/variables/variableset.js
+++ src/variables/variableset.js
@@ -3,14 +3,52 @@
 //main json file that contains variable names
 var names = require("./names.json");
 
-var randSelect = function(arr){
-	return arr[Math.floor((Math.random() * arr.length-1) + 0)];
-};
 
 var VariableSet = (function(){
+	//private function to randomly select from array
+	var randSelect = function(arr){
+		return arr[Math.floor((Math.random() * arr.length-1) + 1)];
+	};
 	function VariableSet(){
-		
+		this.set = {};
+		this.list = [];
+		this.title = false;
 	}
+	VariableSet.prototype.contains = function(variable) {
+		return variable in this.set;
+	};
+	//creates an entirely new randomly chosen variable name
+	VariableSet.prototype.newVar = function() {
+		var newvariable = randSelect(names.names);
+		while (newvariable in this.set) {
+			newvariable = randSelect(names.names);
+		}
+		this.set[newvariable] = 1;
+		this.list.push(newvariable);
+		return newvariable;
+ 	};
+ 	//picks a randomly existing variable
+ 	VariableSet.prototype.pickVar = function() {
+ 		var picked = randSelect(this.list);
+ 		this.set[picked] += 1;
+ 		return picked;
+ 	};
+ 	//decreases the reference count of a variable
+ 	VariableSet.prototype.decRef = function(variable){
+ 		if(variable in this.set) {
+ 			if(this.set[variable] === 1) {
+ 				delete this.set[variable];
+ 				for (var i = this.list.length - 1; i >= 0; i--) {
+ 					if(this.list[i] === variable) {
+ 						this.list.splice(i, 0);
+ 					}
+ 				}
+ 			}
+ 			else {
+ 				this.set[variable] -= 1;
+ 			}
+ 		}
+ 	};
 	return VariableSet;
 })();
 
